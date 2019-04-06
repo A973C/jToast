c6ad3344ac048dfad6d505fa7579aa4a17788a9c
Leonard Ginters <leo@l2g.cc>
2019-04-06 18:18:55 +0200

    Update jToast.js

--- jToast.js
+++ jToast.js
@@ -2,7 +2,7 @@ let count = 0;
 let id = 0;
 
 $("head").append(`<style>
-    .toast { background: #222; padding: 15px; color: #fff; position: fixed; right: -100%; top: 5%; line-height: 16px; height: 46px; z-index: 1000000; }
+    .toast { background: #222; padding: 15px; color: #fff; position: fixed; right: -100%; top: 5%; z-index: 1000000; }
     @keyframes show {
         from { right: -100% } to { right: 5% }
     }
@@ -14,7 +14,7 @@ $("head").append(`<style>
 </style>`);
 
 const showToast = (text, border_radius, time) => {
-    $("body").append(`<div id="toast_${id}" class="toast" style="margin-top: ${count*61}px; border-radius: ${border_radius};">${text}</div>`);
+    $("body").append(`<div id="toast_${id}" class="toast" style="margin-top: ${count*60}px; border-radius: ${border_radius};">${text}</div>`);
 
     $(`#toast_${id}`).addClass("show");
 
